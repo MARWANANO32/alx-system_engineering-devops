9a024a51ced249e2f3c96284171fb3de1c2e02bd
MARWANANO32 <marwanano32@gmail.com>
2024-05-08 20:50:09 +0300

    MARWAN MOSTAFA

--- 0x16-api_advanced/tests/0-main.py
+++ 0x16-api_advanced/tests/0-main.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-""" Main file """
-
+"""
+0-main
+"""
 import sys
 
-if __name__ == "__main__":
-    number_of_subcribers = __import__('0-subs').number_of_subcribers
+if __name__ == '__main__':
+    number_of_subscribers = __import__('0-subs').number_of_subscribers
     if len(sys.argv) < 2:
         print("Please pass an argument for the subreddit to search.")
     else:
